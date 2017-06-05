2b7e5a5a892940855bf872c3827b615297614acd
Teethemes <info@teethemes.com>
2017-06-05 20:42:13 +0300

    Fix links

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -5,7 +5,10 @@ import Helmet from 'react-helmet'
 import { config } from 'config'
 const styles={
   link:{
-    textDecoration:'none',
+    color:'black',
+    fontFamily:'Ubuntu'
+  },
+  text:{
     color:'black',
     fontFamily:'Ubuntu'
   }
@@ -17,15 +20,15 @@ export default class Index extends React.Component {
         <h1>
           Portfolio
         </h1>
-        <ul>
-          <li><Link style={styles.link} to='http://teethemes.com:3000'> Web application(in progress) based on NodeJs, Angular 1.5, mongoDb</Link></li>
-          <li><Link style={styles.link} to='http://sowdan.com'>Sowdan - mobile applications with ionic</Link></li>
-          <li><Link style={styles.link} to='http://themeforest.net/item/bishop-opencart-responsive-theme/11607490'>Bishop - opencart template</Link></li>
-          <li><Link style={styles.link} to='http://teethemes.com'>T-themes - landing page</Link></li>
-          <li><Link style={styles.link} to='http://themeforest.net/item/the-blazze-blog-and-magazine-html-site-template/11251890'>The Blazze - HTML template</Link></li>
-          <li><Link style={styles.link} to='https://creativemarket.com/teethemes/635708-One-fashion-day'>One fashion Day - Under construction template</Link></li>
-          <li><Link style={styles.link} to='http://templates.teethemes.com/megaplace/'>Megaplace - ecommecrce template</Link></li>
-          <li><Link style={styles.link} to='http://stereolab.com.ua/'>Stereolab - opencart ecommerce website</Link></li>
+        <ul style={styles.text}>
+          <li><a style={styles.link} href='http://teethemes.com:3000'> Web application(in progress)</a> based on NodeJs, Angular 1.5, mongoDb</li>
+          <li><a style={styles.link} href='http://sowdan.com'>Sowdan</a> - mobile applications with ionic</li>
+          <li><a style={styles.link} href='http://themeforest.net/item/bishop-opencart-responsive-theme/11607490'>Bishop</a> - opencart template</li>
+          <li><a style={styles.link} href='http://teethemes.com'>T-themes</a> - landing page</li>
+          <li><a style={styles.link} href='http://themeforest.net/item/the-blazze-blog-and-magazine-html-site-template/11251890'>The Blazze</a> - HTML template</li>
+          <li><a style={styles.link} href='https://creativemarket.com/teethemes/635708-One-fashion-day'>One fashion Day</a> - Under construction template</li>
+          <li><a style={styles.link} href='http://templates.teethemes.com/megaplace/'>Megaplace</a> - ecommecrce template</li>
+          <li><a style={styles.link} href='http://stereolab.com.ua/'>Stereolab</a> - opencart ecommerce website</li>
         </ul>       
       </div>
     )
